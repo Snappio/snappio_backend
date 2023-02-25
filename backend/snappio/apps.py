32afe330b20dea06b31c322137421e5a93e32a76
Ananyobrata Pal <ananyo141@gmail.com>
2023-02-26 00:45:21 +0530

    format boilerplate code with black

--- backend/snappio/apps.py
+++ backend/snappio/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SnappioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'snappio'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "snappio"

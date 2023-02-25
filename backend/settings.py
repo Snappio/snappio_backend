3d549f29374a240596e99f0658430e8b513cf1a0
Ananyobrata Pal <ananyo141@gmail.com>
2023-02-26 01:50:12 +0530

    Add Dotenv feat:  - add dotenv configuration  - add .env.example

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from .environment import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-(_jr$do%4(dtt3!1ry#o+cs^4_@(t+v_nr*bj4g*x2-9pt^$%)"
-)
+SECRET_KEY = env.get("DJANGO_SECRET")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

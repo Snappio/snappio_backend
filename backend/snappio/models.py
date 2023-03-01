6434557472a83ef80d0609ccb4168210d4d56d3f
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-02 00:32:23 +0530

    add post model

--- backend/snappio/models.py
+++ backend/snappio/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} posted at {self.timestamp}"

ae15daca32d86662fb2d9ee8d90dfdd8e9160773
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-04 13:37:49 +0530

    fix poetry build script error

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit
 
 poetry install
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+poetry run ./manage.py collectstatic --no-input
+poetry run ./manage.py makemigrations snappio
+poetry run ./manage.py migrate

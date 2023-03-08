5801326bff28f91721b045e9f95618f535592512
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-08 15:51:51 +0530

    Fix import sequence for django setup in production

--- backend/asgi.py
+++ backend/asgi.py
@@ -13,12 +13,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from .chat.auth import TokenAuthMiddleware
-from .chat.urls import urlpatterns as websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 django_asgi_app = get_asgi_application()
+
+# import after django setup
+from .chat.auth import TokenAuthMiddleware
+from .chat.urls import urlpatterns as websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {

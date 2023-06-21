bce2be3e39457f354501b50b942562acba06c22e
Ananyobrata Pal <ananyo141@gmail.com>
2023-06-21 11:41:46 +0530

    use python3 in build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-./manage.py collectstatic --no-input
-./manage.py makemigrations snappio
-./manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations snappio
+python3 manage.py migrate
